aa3e375e56516b260ac078a3d4bd2a363c4d97fa
Andy Bursavich <abursavich@gmail.com>
2020-08-02 18:41:59 -0700

    discovery: invert dependency on dns
    Signed-off-by: Andy Bursavich <abursavich@gmail.com>

--- discovery/dns/dns.go
+++ discovery/dns/dns.go
@@ -28,6 +28,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/model"
 
+	"github.com/prometheus/prometheus/config"
+	"github.com/prometheus/prometheus/discovery/discoverer"
 	"github.com/prometheus/prometheus/discovery/refresh"
 	"github.com/prometheus/prometheus/discovery/targetgroup"
 )
@@ -65,6 +67,12 @@ var (
 	}
 )
 
+func init() {
+	config.RegisterServiceDiscovery(&SDConfig{})
+	prometheus.MustRegister(dnsSDLookupFailuresCount)
+	prometheus.MustRegister(dnsSDLookupsCount)
+}
+
 // SDConfig is the configuration for DNS based service discovery.
 type SDConfig struct {
 	Names           []string       `yaml:"names"`
@@ -72,6 +80,20 @@ type SDConfig struct {
 	Type            string         `yaml:"type"`
 	Port            int            `yaml:"port"` // Ignored for SRV records
 }
+
+// Name returns the name of the Config.
+func (*SDConfig) Name() string { return "dns" }
+
+// NewDiscoverer returns a Discoverer for the Config.
+func (c *SDConfig) NewDiscoverer(opts discoverer.Options) (discoverer.Discoverer, error) {
+	return NewDiscovery(*c, opts.Logger), nil
+}
+
+// SetOptions applies the options to the Config.
+func (c *SDConfig) SetOptions(opts discoverer.ConfigOptions) {}
+
+// Validate checks the Config for errors.
+func (*SDConfig) Validate() error { return nil }
 
 // UnmarshalYAML implements the yaml.Unmarshaler interface.
 func (c *SDConfig) UnmarshalYAML(unmarshal func(interface{}) error) error {
@@ -94,11 +116,6 @@ func (c *SDConfig) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		return errors.Errorf("invalid DNS-SD records type %s", c.Type)
 	}
 	return nil
-}
-
-func init() {
-	prometheus.MustRegister(dnsSDLookupFailuresCount)
-	prometheus.MustRegister(dnsSDLookupsCount)
 }
 
 // Discovery periodically performs DNS-SD requests. It implements
